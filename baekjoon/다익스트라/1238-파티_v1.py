c9e669ede79fc1e71016c26a748539faa82d0922
jongseong <kjs3870@naver.com>
2021-03-08 15:16:48 +0900

    baekjoon / solved 1238

---
+++ baekjoon/다익스트라/1238-파티_v1.py
@@ -0,0 +1,36 @@
+# 992ms
+
+import sys
+from collections import deque
+
+INF = float('inf')
+input = sys.stdin.readline
+
+n, m, x = map(int, input().rsplit())
+_map = [[] for _ in range(n+1)]
+for _ in range(m):
+    _from, _to, time = map(int, input().rsplit())
+    _map[_from].append([_to, time])
+
+def dijkstra(start):
+    costs = [INF] * (n+1)
+    que = deque()
+    que.append([start, 0])
+    costs[start] = 0
+
+    while que:
+        city, time = que.popleft()
+        for nxt, t in _map[city]:
+            nxt_time = time + t
+            if costs[nxt] > nxt_time:
+                costs[nxt] = nxt_time
+                que.append([nxt, nxt_time])
+    
+    return costs
+
+home2party = [dijkstra(i)[x] for i in range(1, n+1)]
+party2home = dijkstra(x)[1:]
+
+round_trip = list(map(sum,zip(home2party, party2home)))
+
+print(max(round_trip))
