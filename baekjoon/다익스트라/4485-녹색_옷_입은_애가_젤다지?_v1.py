96858a1d1cfb0d611568f558cfd519e40b319a28
jongseong-mac <kjs3870@naver.com>
2021-04-18 16:52:35 +0900

    baekjoon / solved 4485

--- baekjoon/다익스트라/4485-녹색_옷_입은_애가_젤다지?_v1.py
+++ baekjoon/다익스트라/4485-녹색_옷_입은_애가_젤다지?_v1.py
@@ -1,3 +1,5 @@
+# 228ms
+
 import sys
 from collections import deque
 
@@ -20,7 +22,7 @@ while True:
 
     while q:
         y, x = q.popleft()
-        # print(y, x, dist[y][x])
+
         for wy, wx in WAY:
             ny = y + wy
             nx = x + wx
@@ -31,23 +33,3 @@ while True:
         
     print("Problem {0}: {1}".format(t, dist[n-1][n-1]))
     t += 1
-
-# 3
-# 5 5 4
-# 3 9 1
-# 3 2 7
-# 5
-# 3 7 2 0 1
-# 2 8 0 9 1
-# 1 2 1 8 1
-# 9 8 9 2 0
-# 3 6 5 1 5
-# 7
-# 9 0 5 1 1 5 3
-# 4 1 2 1 6 5 3
-# 0 7 6 1 6 8 5
-# 1 1 7 8 3 2 3
-# 9 4 0 7 6 4 1
-# 5 8 3 2 4 8 3
-# 7 4 8 4 8 3 4
-# 0
