ec31af6b4717e006ed11fa5a5736d3a422d9769e
jongseong-mac <kjs3870@naver.com>
2022-04-01 12:37:59 +0900

    programmers / solved 행렬 테두리 회전하기 java

--- programmers/LEVEL-2/행렬_테두리_회전하기.java
+++ programmers/LEVEL-2/행렬_테두리_회전하기.java
@@ -1,67 +1,55 @@
 /**
  * LEVEL-2
- * 2021 Dev_Matching: 웹 백엔드 > 행렬 테두리 회전하기
+ * 2021 Dev-Matching: 웹 백엔드 개발자(상반기) - 행렬 테두리 회전하기
+ * https://programmers.co.kr/learn/courses/30/lessons/77485
  */
 public class Solution {
-    private int[][] board;
 
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
-
-        init(rows, columns);
+        int[][] matrix = new int[rows + 1][columns + 1];
+        int elem = 1;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                matrix[i][j] = elem++;
+            }
+        }
 
         for (int i = 0; i < queries.length; i++) {
-            answer[i] = rotate(queries[i]);
+            answer[i] = rotate(matrix, queries[i]);
         }
 
         return answer;
     }
 
-    public void init(int rows, int columns) {
-        board = new int[rows][columns];
+    public int rotate(int[][] matrix, int[] query) {
+        int x1 = query[0], y1 = query[1];
+        int x2 = query[2], y2 = query[3];
 
-        int number = 1;
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < columns; j++)
-                board[i][j] = number++;
-    }
-
-    public int rotate(int[] query) {
-        int y1 = query[0] - 1;
-        int x1 = query[1] - 1;
-        int y2 = query[2] - 1;
-        int x2 = query[3] - 1;
-
-        int temp = board[y1][x1];
-        int minValue = Integer.MAX_VALUE;
-
-        // left
-        for (int y = y1; y < y2; y++) {
-            board[y][x1] = board[y+1][x1];
-            minValue = Math.min(minValue, board[y][x1]);
+        int temp = matrix[x1][y1];
+        int min = temp;
+        for (int i = x1; i < x2; i++) {
+            matrix[i][y1] = matrix[i + 1][y1];
+            min = Math.min(min, matrix[i][y1]);
         }
 
-        // down
-        for (int x = x1; x < x2; x++) {
-            board[y2][x] = board[y2][x+1];
-            minValue = Math.min(minValue, board[y2][x]);
+        for (int i = y1; i < y2; i++) {
+            matrix[x2][i] = matrix[x2][i + 1];
+            min = Math.min(min, matrix[x2][i]);
         }
 
-        // right
-        for (int y = y2; y > y1; y--) {
-            board[y][x2] = board[y-1][x2];
-            minValue = Math.min(minValue, board[y][x2]);
+        for (int i = x2; i > x1; i--) {
+            matrix[i][y2] = matrix[i - 1][y2];
+            min = Math.min(min, matrix[i][y2]);
         }
 
-        // up
-        for (int x = x2; x > x1; x--) {
-            board[y1][x] = board[y1][x-1];
-            minValue = Math.min(minValue, board[y1][x]);
+        for (int i = y2; i > y1; i--) {
+            matrix[x1][i] = matrix[x1][i - 1];
+            min = Math.min(min, matrix[x1][i]);
         }
 
-        board[y1][x1+1] = temp;
-        minValue = Math.min(minValue, temp);
+        matrix[x1][y1 + 1] = temp;
 
-        return minValue;
+        return min;
     }
 }
