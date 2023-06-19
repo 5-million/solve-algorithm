b9242995c39bde26c023ac0f26594ced792a9ddf
jongseong <eello.bdev@gmail.com>
2023-06-19 11:28:55 +0900

    PRGRMS LV2::마법의 엘리베이터 JAVA

--- programmers/LEVEL-2/마법의_엘리베이터.java
+++ programmers/LEVEL-2/마법의_엘리베이터.java
@@ -1,6 +1,30 @@
 class Solution {
+    
     public int solution(int storey) {
-        int answer = 0;
-        return answer;
+        return dfs(storey, 0);
+    }
+    
+    private int dfs(int storey, int click) {
+        if (storey == 0) {
+            return click;
+        }
+        
+        int ret = Integer.MAX_VALUE;
+        int temp = storey % 10;
+        if (temp > 5) {
+            temp = 10 - temp;
+            storey += temp;
+            storey /= 10;
+            return dfs(storey, click + temp);
+        } else if (temp < 5) {
+            storey -= temp;
+            storey /= 10;
+            return dfs(storey, click + temp);
+        } else {
+            return Math.min(
+                dfs((storey + temp) / 10, click + 5),
+                dfs((storey - temp) / 10, click + 5)
+            );
+        }
     }
 }
