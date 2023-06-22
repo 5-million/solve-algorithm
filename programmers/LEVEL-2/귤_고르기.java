c79ba9e3737f37fc94c19b9c44543dcae1b3231f
jongseong <eello.bdev@gmail.com>
2023-06-22 22:42:49 +0900

    PRGRMS LV2::귤 고르기 JAVA

---
+++ programmers/LEVEL-2/귤_고르기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int t : tangerine) {
+			map.put(t, map.getOrDefault(t, 0) + 1);
+		}
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+		pq.addAll(map.values());
+
+		int answer = 0;
+		while (k > 0) {
+			k -= pq.poll();
+            answer++;
+		}
+		
+		return answer;
+	}
+}
