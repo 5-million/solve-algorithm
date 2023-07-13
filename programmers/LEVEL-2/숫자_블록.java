af10a0c6b9bf68a13d1bfc46ccd36fe2bfc002b1
jongseong <eello.bdev@gmail.com>
2023-07-13 14:24:13 +0900

    PRGRMS LV2::숫자 블록 JAVA

--- programmers/LEVEL-2/숫자_블록.java
+++ programmers/LEVEL-2/숫자_블록.java
@@ -1,6 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
+    
     public int[] solution(long begin, long end) {
-        int[] answer = {};
-        return answer;
-    }
+		int len = (int)(end - begin + 1);
+		int[] answer = new int[len];
+		Arrays.fill(answer, 1);
+
+		for (long i = begin; i <= end; i++) {
+			int idx = (int)(i - begin);
+
+			int sqrt = (int)Math.sqrt(i);
+			int val = 1;
+			for (int j = 2; j <= sqrt; j++) {
+				if (i % j == 0) {
+					val = j;
+					int temp = (int)(i / j);
+
+					if (temp <= 10000000) {
+						val = temp;
+						break;
+					}
+				}
+			}
+			answer[idx] = val;
+		}
+
+		if (begin == 1) {
+			answer[0] = 0;
+		}
+
+		return answer;
+	}
 }
