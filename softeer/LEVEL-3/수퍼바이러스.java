727fd995a8ea8f9f3778ac45a829309d64abb171
jongseong <eello.bdev@gmail.com>
2023-07-27 10:46:52 +0900

    SOFTEER LV3::수퍼바이러스 JAVA

---
+++ softeer/LEVEL-3/수퍼바이러스.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    private static final int MOD = 1000000007;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        long n = Long.parseLong(st.nextToken());
+
+        long answer = k * dnq(p, n* 10) % MOD;
+        System.out.println(answer);
+    }
+
+    private static long dnq(int p, long n) {
+        if (n == 1) {
+            return p;
+        }
+
+        if (n % 2 != 0) {
+            return p * dnq(p, n - 1) % MOD;
+        }
+
+        long temp = dnq(p, n / 2);
+        return temp * temp % MOD;
+    }
+}
