e27e04f1c7f1fe1931e090c18c8bf88272fddfe9
jongseong <eello.bdev@gmail.com>
2023-09-01 14:54:32 +0900

    LEETCODE EASY::155 - Min Stack JAVA

---
+++ leetcode/top-interview-150/155-Min-Stack.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    private static final int MAX_SIZE = 30000;
+    private int[] stack = new int[MAX_SIZE + 1];
+    private int[] minStack = new int[MAX_SIZE + 1];
+    private int top = -1, minTop = -1;
+
+
+    public MinStack() {
+    }
+    
+    public void push(int val) {
+        stack[++top] = val;
+        
+        if (minTop == - 1 || minStack[minTop] >= val) {
+            minStack[++minTop] = val;
+        }
+    }
+    
+    public void pop() {
+        if (stack[top] == minStack[minTop]) {
+            minTop--;
+        }
+        top--;
+    }
+    
+    public int top() {
+        return stack[top];
+    }
+    
+    public int getMin() {
+        return minStack[minTop];
+    }
+}
