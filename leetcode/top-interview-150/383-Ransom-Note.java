71e00b2aa7071e83e17ff105bfaf43d28b797d01
jongseong <eello.bdev@gmail.com>
2023-09-01 14:59:01 +0900

    LEETCODE EASY::383 - Ransom Note JAVA

---
+++ leetcode/top-interview-150/383-Ransom-Note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] letters = new char[26];
+        for (char ch : magazine.toCharArray()) {
+            letters[ch - 'a']++;
+        }
+
+        for (char ch : ransomNote.toCharArray()) {
+            int idx = ch - 'a';
+            if (letters[idx] == 0) {
+                return false;
+            }
+
+            letters[idx]--;
+        }
+
+        return true;
+    }
+}
