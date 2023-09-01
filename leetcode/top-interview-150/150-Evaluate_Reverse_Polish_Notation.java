83cb6a2c4faa647a241a34105a4b020674001bfc
jongseong <eello.bdev@gmail.com>
2023-09-01 14:57:21 +0900

    LEETCODE MEDIUM::150 - Evaluate Reverse Polish Notation JAVA

---
+++ leetcode/top-interview-150/150-Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (!isOperator(token)) {
+                stack.push(Integer.parseInt(token));
+            } else {
+                stack.push(calculate(token, stack.pop(), stack.pop()));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return "+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token);
+    }
+
+    private int calculate(String operator, int num2, int num1) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            default:
+                return num1 / num2;
+        }
+    }
+}
