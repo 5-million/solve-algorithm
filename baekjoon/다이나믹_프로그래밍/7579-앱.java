6f110753f55237b2142f3907cee0d6bd9b3512e2
jongseong <eello.bdev@gmail.com>
2024-01-18 15:50:04 +0900

    BOJ G3::7579 - 앱 JAVA

--- baekjoon/다이나믹_프로그래밍/7579-앱.java
+++ baekjoon/다이나믹_프로그래밍/7579-앱.java
@@ -1,52 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * G3
- * 7579 - 앱
- * https://www.acmicpc.net/problem/7579
- */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()); // 실행 중인 앱의 개수
+		int m = Integer.parseInt(st.nextToken()); // 확보해야하는 메모리
 
-        st = new StringTokenizer(br.readLine());
-        int[] memory = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            memory[i] = Integer.parseInt(st.nextToken());
-        }
+		int[] memory = new int[n]; // i번째 앱이 사용하고 있는 메모리
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			memory[i] = Integer.parseInt(st.nextToken());
+		}
 
-        st = new StringTokenizer(br.readLine());
-        int[] cost = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
-        }
+		int[] cost = new int[n]; // i번째 앱의 비활성화 비용
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			cost[i] = Integer.parseInt(st.nextToken());
+		}
 
-        int ans = Integer.MAX_VALUE;
-        int[][] dp = new int[n + 1][n * 100 + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n * 100; j++) {
-                if (j < cost[i]) {
-                    dp[i][j] = dp[i - 1][j];
-                    continue;
-                }
+		int ans = Integer.MAX_VALUE;
+		int maxCost = n * 100 + 1;
+		int[][] dp = new int[n + 1][maxCost];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 0; j < maxCost; j++) {
+				if (j < cost[i - 1]) {
+					dp[i][j] = dp[i - 1][j];
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost[i - 1]] + memory[i - 1]);
 
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost[i]] + memory[i]);
+					if (dp[i][j] >= m) {
+						ans = Math.min(ans, j);
+					}
+				}
+			}
+		}
 
-                if (dp[i][j] >= m) {
-                    ans = Math.min(ans, j);
-                    break;
-                }
-            }
-        }
-
-        System.out.println(ans);
-    }
+		System.out.println(ans);
+	}
 }
